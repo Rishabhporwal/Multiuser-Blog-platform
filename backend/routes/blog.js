b9a602ffabea8afdbe5373e8a564900af9421e62
Rishabh Porwal <rishabhporwal95@gmail.com>
2020-09-28 00:38:54 +0530

    related blogs tags categories all basic functionalities completed

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/blogs");
+const {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update,
+  photo,
+  listRelated
+} = require("../controllers/blogs");
 const {
   requireSignin,
   authMiddleware,
@@ -9,5 +18,12 @@ const {
 } = require("../controllers/auth");
 
 router.post("/blog", requireSignin, adminMiddleware, create);
+router.get("/blogs", list);
+router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
+router.get("/blogs/:slug", read);
+router.delete("/blogs/:slug", requireSignin, adminMiddleware, remove);
+router.put("/blogs/:slug", requireSignin, adminMiddleware, update);
+router.get('/blog/photo/:slug', photo)
+router.post('/blogs/related', listRelated)
 
 module.exports = router;
--- frontend/actions/blog.js
+++ frontend/actions/blog.js
@@ -17,3 +17,50 @@ export const createBlog = (blog, token) => {
       console.log(err);
     });
 };
+
+export const listBlogsWithCategoriesAndTags = (skip, limit) => {
+  const data = { limit, skip };
+  return fetch(`${API}/blogs-categories-tags`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const singleBlog = (slug) => {
+  return fetch(`${API}/blogs/${slug}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const listRelated = (blog) => {
+  return fetch(`${API}/blogs/related`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blog),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
